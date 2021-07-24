fde91e1a4c39e541dc8307fdfb1db7fad64fcc10
deepuboy <86372847+deepuboy@users.noreply.github.com>
2021-07-24 15:37:49 +0530

    password validation

--- form.js
+++ form.js
@@ -1,6 +1,33 @@
+function fun(){
+    console.log("here");
+    var a = document.getElementById("Password").value;
+    var b = document.getElementById("ConfirmPassword").value;
+    if(a.length<8){
+        document.getElementById("span1").innerHTML="**Password should be at least of 8 chracters**";
+    }
+        else if(a.length>15)
+        {
+            document.getElementById("span1").innerHTML="**Password should be less than 15 chracters**";
+        }
+     
+    
+   else if(a!=b){
+        document.getElementById("span1").innerHTML="**Password is not matched**";
 
-function Name1 (){
-    let ID1 = document.getElementById('Name');
-    ID1.style.backgroundColor = "Blue" 
+    }
+    
+    else{
+        alert("Password and Confirm Password is same");
 
-}
+    }
+    
+
+
+        
+    }
+
+//min 8 , max 15 
+//confirm pass == password
+//==> alert("password and confirm password is same")
+
+//if(a.length> && a.length1<5)
